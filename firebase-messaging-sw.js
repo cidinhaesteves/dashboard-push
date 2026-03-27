eacc83864d41080463128a5fe40fb64702664c9a
cidinhaesteves <cidinhaesteves2014@gmail.com>
2026-03-26 23:50:48 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
+firebase.initializeApp({
+  apiKey: "AIzaSyCN0S5pMthAbPyC1LkYy-t2xFDoc8QuR8A",
+  authDomain: "eco-goias.firebaseapp.com",
+  projectId: "eco-goias",
+  storageBucket: "eco-goias.firebasestorage.app",
+  messagingSenderId: "31870991742",
+  appId: "1:31870991742:web:2ea58294b93441b47c5d4f",
+  measurementId: "G-RJVTCZ41QX"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Push recebido:', payload);
+
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+  });
+});
