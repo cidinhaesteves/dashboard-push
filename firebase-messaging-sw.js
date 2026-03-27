a3cc021c6bb8a1dfb288e51a392b686eb738c209
cidinhaesteves <cidinhaesteves2014@gmail.com>
2026-03-27 15:09:12 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,22 @@
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCN0S5pMthAbPyC1LkYy-t2xFDoc8QuR8A",
+  apiKey: "AIzaSyCJzv86uRLFRAX1CBVejno1_89VD90J30Y",
   authDomain: "eco-goias.firebaseapp.com",
   projectId: "eco-goias",
   storageBucket: "eco-goias.firebasestorage.app",
-  messagingSenderId: "31870991742",
-  appId: "1:31870991742:web:2ea58294b93441b47c5d4f",
-  measurementId: "G-RJVTCZ41QX"
+  messagingSenderId: "31870091742",
+  appId: "1:31870091742:web:f75d7509b351bfac7c5d4f"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  console.log('Push recebido:', payload);
+  console.log("📩 BACKGROUND:", payload);
 
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
+    icon: "/icon.png"
   });
 });
