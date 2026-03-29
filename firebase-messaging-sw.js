58a0a09b3c1f7fcfec46f1b72423085ad662de20
cidinhaesteves <cidinhaesteves2014@gmail.com>
2026-03-29 08:52:25 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,21 +2,34 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-apiKey: "AIzaSyCN05SpMfHAbPyCl1kYy-t2xFDoc8QuR8A",
-authDomain: "eco-goias.firebaseapp.com",
-projectId: "eco-goias",
-storageBucket: "eco-goias.firebasestorage.app",
-messagingSenderId: "31870091742",
-appId: "1:31870091742:web:2ea58294b93441b47c5d4f"
+  apiKey: "AIzaSyCJzv86uRlFRAX1CBVejno1_89VD90J3OY",
+  authDomain: "eco-goias.firebaseapp.com",
+  projectId: "eco-goias",
+  storageBucket: "eco-goias.firebasestorage.app",
+  messagingSenderId: "31870091742",
+  appId: "1:31870091742:web:f75d7509b351bfac7c5d4f"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-console.log("📩 PUSH RECEBIDO:", payload);
+// 🔥 ESSENCIAL — RECEBER PUSH
+self.addEventListener("push", function (event) {
+  console.log("🔥 PUSH RECEBIDO:", event);
 
-self.registration.showNotification(payload.notification.title, {
-body: payload.notification.body,
-icon: "https://cdn-icons-png.flaticon.com/512/1827/1827392.png"
-});
+  if (!event.data) return;
+
+  const data = event.data.json();
+
+  const title = data.title || "Nova notificação";
+  const body = data.body || "Você recebeu uma mensagem";
+
+  const options = {
+    body: body,
+    icon: "https://cdn-icons-png.flaticon.com/512/1827/1827392.png",
+    badge: "https://cdn-icons-png.flaticon.com/512/1827/1827392.png"
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
 });
