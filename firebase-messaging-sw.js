42d239aa8c1bc562f0a06b716d474887652715ed
cidinhaesteves <cidinhaesteves2014@gmail.com>
2026-03-29 23:56:54 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,32 +1,29 @@
-importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js");
+self.addEventListener("push", function (event) {
+  console.log("🔥 PUSH RECEBIDO");
 
-// ==============================
-// FIREBASE CONFIG (SEU PROJETO)
-// ==============================
-firebase.initializeApp({
-  apiKey: "AIzaSyCJzv86uRLFRAX1CBVejno1_89VD90J3OY",
-  authDomain: "eco-goias.firebaseapp.com",
-  projectId: "eco-goias",
-  storageBucket: "eco-goias.firebasestorage.app",
-  messagingSenderId: "31870091742",
-  appId: "1:31870091742:web:f75d7509b351bfac7c5d4f",
-  measurementId: "G-G286NDQQJZ"
-});
+  if (!event.data) {
+    console.log("❌ Sem dados");
+    return;
+  }
 
-const messaging = firebase.messaging();
+  let data = {};
 
-// ==============================
-// RECEBER PUSH (FIREBASE CORRETO)
-// ==============================
-messaging.onBackgroundMessage(function (payload) {
-  console.log("🔥 PUSH RECEBIDO:", payload);
+  try {
+    data = event.data.json();
+  } catch (e) {
+    console.log("❌ Erro ao ler JSON");
+    return;
+  }
 
-  const title = payload.data?.title || "Nova mensagem";
-  const body = payload.data?.body || "Você tem uma notificação";
+  console.log("📩 DATA:", data);
 
-  self.registration.showNotification(title, {
-    body: body,
-    icon: "https://cdn-icons-png.flaticon.com/512/1827/1827392.png"
-  });
+  const title = data.title || "Nova mensagem";
+  const body = data.body || "Você tem uma notificação";
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: "https://cdn-icons-png.flaticon.com/512/1827/1827392.png"
+    })
+  );
 });
